c3ae2b44dac4288dd883b9569b4f56544218d2fb
Suneth Perera <sunethjoes@gmail.com>
2020-09-06 00:26:11 +1000

    object type to addToCollection

--- NaatiGeekAdmin/src/app/Services/firebase.service.ts
+++ NaatiGeekAdmin/src/app/Services/firebase.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
-import WordDoc from '../Services/word-doc.model';
+import { WordDoc } from '../Services/word-doc.model';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +10,7 @@ export class FirebaseService {
 
   constructor(private afs: AngularFirestore) { }
 
-  cpAddToCollection(pStrCollection, pData:Object) :void { 
+  cpAddToCollection(pStrCollection, pData) :void { 
     const shirtsCollection = this.afs.collection<WordDoc>(pStrCollection);
     shirtsCollection.add(pData);
   }
